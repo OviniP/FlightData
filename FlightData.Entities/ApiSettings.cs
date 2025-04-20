74d5e1e088f1659d107ee020ac28928c40bf41c8
OviniP <ovinipoornima@gmail.com>
2025-04-20 23:20:51 +0300

    settings file added

---
+++ FlightData.Entities/ApiSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightData.Entities
+{
+    public class ApiSettings
+    {
+        [Required(ErrorMessage = "The 'DataFilePath' field is required.")]
+        public required string DataFilePath { get; set; }
+    }
+}
